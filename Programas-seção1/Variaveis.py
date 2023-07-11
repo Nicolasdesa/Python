2050acca59a536aacfd2daade6d87f64161a611b
Nicolas de Sá <nicolasdesacs@gmail.com>
2023-07-11 08:27:09 -0300

    Criei o código de variáveis
    Ex. de fixação do modulo da aula de variáveis

---
+++ Programas-seção1/Variaveis.py
@@ -0,0 +1,13 @@
+nome = "Nicolas"
+sobrenome = "de Sá"
+idade = 20
+ano_nascimento = 2003
+maior_de_idade = idade >= 18
+altura_metros = 1.78
+
+print("Nome:", nome)
+print("Sobrenome:", sobrenome)
+print("Idade:", idade)
+print("Ano de nascimento:", ano_nascimento)
+print("É maior de idade?", maior_de_idade)
+print("Altura em metros:", altura_metros)
