445c105803e51f200ef7c77cdd71fb2f4fd59f69
Nicolas de Sá <nicolasdesacs@gmail.com>
2023-07-21 08:43:58 -0300

    Inicio programa calculadora
    Algoritmo para sair do programa

---
+++ Programas-seção1/8_calculadora_while.py
@@ -0,0 +1,10 @@
+# Exercício de fixação dos blocos de repetição
+# Objetivo: criar uma calculadora com while
+
+while True:
+    ...
+
+    sair = input("Quer sair? [s]im").lower().startswith("s")
+
+    if sair is True:
+        break
