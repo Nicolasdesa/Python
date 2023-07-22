7b2dd93629364f16f1a81c5f076fadbcdc2ee382
Nicolas de Sá <nicolasdesacs@gmail.com>
2023-07-22 16:42:32 -0300

    tentativa 1 - programa 10
    Problema com os * na palavra oculta

--- Programas-seção1/10_Jogo_da_forca.py
+++ Programas-seção1/10_Jogo_da_forca.py
@@ -2,16 +2,21 @@
 # Objetivo: Criar um jogo de forca
 
 palavra_secreta = "programa"
+numero_tentativas = 0
 
 while True:
     tentativa = input("Digite uma letra: ")
-    palavra_oculta = "*" * len(palavra_secreta) 
+    palavra_oculta = ""
+    numero_tentativas += 1 
 
     if len(tentativa) > 1:
         print("Digite somente uma letra.")
         continue
 
     for i in palavra_secreta:
-        if tentativa == i:
-            palavra_oculta[i] = tentativa
+        if tentativa == palavra_secreta[i]:
+            palavra_oculta += tentativa
+        elif palavra_oculta[i] != "*":
+            palavra_oculta += "*"
+    
     print(palavra_oculta)
