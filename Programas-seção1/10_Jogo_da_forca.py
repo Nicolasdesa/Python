5ae4f59092f49613acf16bea2b21bda575303650
Nicolas de Sá <nicolasdesacs@gmail.com>
2023-07-25 07:29:45 -0300

    conclusão programa 10
    Conclusão do jogo forca

--- Programas-seção1/10_Jogo_da_forca.py
+++ Programas-seção1/10_Jogo_da_forca.py
@@ -1,22 +1,34 @@
 # Exercício de fixação de estrutura de repetição (for/in)
 # Objetivo: Criar um jogo de forca
 
+import os
+
 palavra_secreta = "programa"
 numero_tentativas = 0
+letras_acertadas = ""
 
 while True:
     tentativa = input("Digite uma letra: ")
-    palavra_oculta = ""
     numero_tentativas += 1 
 
     if len(tentativa) > 1:
         print("Digite somente uma letra.")
         continue
 
-    for i in palavra_secreta:
-        if tentativa == palavra_secreta[i]:
-            palavra_oculta += tentativa
-        elif palavra_oculta[i] != "*":
-            palavra_oculta += "*"
+    if tentativa in palavra_secreta:
+        letras_acertadas += tentativa
+
+    palavra_formada = ""
+    for letra_secreta in palavra_secreta:
+        if letra_secreta in letras_acertadas:
+            palavra_formada += letra_secreta
+        else:
+            palavra_formada += "*"
     
-    print(palavra_oculta)
+    print(palavra_formada)
+
+    if palavra_formada == palavra_secreta:
+        os.system("cls") #limpar o terminal
+        print(f'Parábens, a palavra é "{palavra_secreta}"!')
+        print(f"Você acertou em {numero_tentativas} tentativas")
+        break
