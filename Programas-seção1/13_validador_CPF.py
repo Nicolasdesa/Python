5e05162a2c30eb5d76952ae79d9cdb4d61d3e911
Nicolas de Sá <nicolasdesacs@gmail.com>
2023-07-31 17:25:25 -0300

    verificador de CPF completo
    Término do código de CPF

--- Programas-seção1/13_validador_CPF.py
+++ Programas-seção1/13_validador_CPF.py
@@ -1,15 +1,32 @@
 # Exercício de conclusão do módulo 1
 # Objetivo: Criar um programa para validar o CPF inserido
 
-cpf = "74682489070"
+cpf_enviado = "74682489070"
 nove_digitos = cpf[:9]  #Separa os 9 diígitos da String, através de fatiamento
-contador_regressivo = 10
+contador_regressivo1 = 10
 soma_cpf = 0
 
 for digito in nove_digitos:
-    soma_cpf += int(digito) * contador_regressivo # Int digito porque era uma Str
-    contador_regressivo -= 1
+    soma_cpf += int(digito) * contador_regressivo1 # Int digito porque era uma Str
+    contador_regressivo1 -= 1
 
 primeiro_digito = (soma_cpf * 10) % 11
 primeiro_digito = primeiro_digito if primeiro_digito <= 9 else 0
 # Linha acima: Se o primeiro digito for maior que 9 então ele é 0
+
+dez_digitos = nove_digitos + str(primeiro_digito)
+contador_regressivo2 = 11
+
+resultado_digito2 = 0
+for digito in dez_digitos:
+    resultado_digito2 += int(digito) * contador_regressivo2
+    contador_regressivo2 -= 1
+segundo_digito = (resultado_digito2 * 10) % 11
+segundo_digito = segundo_digito if segundo_digito <= 9 else 0
+
+cpf_verificador = f"{nove_digitos}{primeiro_digito}{segundo_digito}"
+
+if cpf_enviado == cpf_verificador:
+    print("O CPF enviado é válido")
+else:
+    print("O CPF enviado é invalido")
