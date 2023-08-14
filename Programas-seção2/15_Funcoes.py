42305733997c66d7dc7ca59c59c2ee0de0f3ce86
Nicolas de Sá <nicolasdesacs@gmail.com>
2023-08-14 17:44:37 -0300

    Finalização programa 15
    Programa de funções concluído

--- Programas-seção2/15_Funcoes.py
+++ Programas-seção2/15_Funcoes.py
@@ -5,5 +5,15 @@
 def multiplicar(*args):
     total = 1 # 1 porque qualquer numero * 0 é 0
     for numero in args:
-        total += numero
+        total *= numero
     return total
+
+resultado = multiplicar(1, 3, 4, 6, 23, 4, 7)
+print(resultado)
+
+def impar_ou_par(numero):
+    if numero % 2 == 0:
+        return "O número das multiplicações é par"
+    return "O número das multiplicações é impar"
+    
+print(impar_ou_par(resultado))
