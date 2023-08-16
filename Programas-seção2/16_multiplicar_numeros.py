a3e359a65536ff6ca275e02c1e5c8fe232549d09
Nicolas de Sá <nicolasdesacs@gmail.com>
2023-08-16 17:50:43 -0300

    resolução programa 16
    Utilizando Closure

--- Programas-seção2/16_multiplicar_numeros.py
+++ Programas-seção2/16_multiplicar_numeros.py
@@ -1,3 +1,17 @@
 # Exercício de fixação de funções
 # Objetivo: criar um programa que possa multiplicar qualquer numero em qualquer
 # multiplicador (utilizando high order e closure)
+
+def criar_multiplicador(multiplicador):
+    def multiplicar(numero):
+            resultado = multiplicador * numero
+            return resultado
+    return multiplicar
+
+duplicar = criar_multiplicador(2)
+triplicar = criar_multiplicador(3)
+quadruplicar = criar_multiplicador(4)
+
+print(duplicar(5))
+print(triplicar(5))
+print(quadruplicar(5))
