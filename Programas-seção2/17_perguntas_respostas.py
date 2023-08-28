cea406113185e083ee09983bf68a26d8994da734
Nicolas de Sá <nicolasdesacs@gmail.com>
2023-08-28 17:16:54 -0300

    Conclusão programa 17

--- Programas-seção2/17_perguntas_respostas.py
+++ Programas-seção2/17_perguntas_respostas.py
@@ -19,11 +19,14 @@ perguntas = [
     }
 ]
 
+
+qntd_acertos = 0
 for pergunta in perguntas:
-    print("Pergunta", pergunta["Pergunta"])
+    print("Pergunta:", pergunta["Pergunta"])
     print()
 
-    for i, opcao in enumerate(pergunta["Opções"]):
+    opcoes = pergunta["Opções"]
+    for i, opcao in enumerate(opcoes):
         print(f"{i})", opcao)
     print()
 
@@ -31,8 +34,20 @@ for pergunta in perguntas:
 
     acertou = False
     escolha_int = None
+    qntd_opcoes = len(opcoes)
 
     if escolha.isdigit():
         escolha_int = int(escolha)
     
     if escolha_int is not None:
+        if escolha_int >= 0 and escolha_int < qntd_opcoes:
+            if opcoes[escolha_int] == pergunta["Resposta"]:
+                acertou = True
+
+    if acertou:
+        qntd_acertos += 1
+        print("Acertou")
+    else:
+        print("Errou")
+    
+print(f"Você acertou {qntd_acertos} vezes de {len(perguntas)}")
